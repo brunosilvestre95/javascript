b942ab3d37e4aa4018456d68fe99d376105d4335
brunosilvestre95 <153228287+brunosilvestre95@users.noreply.github.com>
2024-07-07 21:04:29 -0300

    att
    att exercício horario

--- exercicios/aula12ex/ex014/js/js-ex12/script.js
+++ exercicios/aula12ex/ex014/js/js-ex12/script.js
@@ -1,1 +1,21 @@
 alert ('Olá Mundo!')
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+                //BOM DIA!
+        img.src = 'foto-dia.png'
+        document.body.style.background = '#ffe656'
+    } else if (hora >= 12 && hora < 18) {
+                //BOA TARDE!
+        img.src = 'foto-tarde.png'
+        document.body.style.background = '#fbb77b'
+    } else {
+                //BOA NOITE!
+    img.src = 'foto-noite.png'
+    document.body.style.background = '#3e335d'
+    }
+}
